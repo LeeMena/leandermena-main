000b3ed264f139fff911c7a1a5a81dc6d8e90123
LeeMena <leandermena@me.com>
2026-05-30 00:18:35 -0400

    fix: remove BOM from App.tsx, fix _redirects format, align routes to insights/* paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-﻿import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Layout from '@/components/Layout'
 import Home from '@/pages/Home'
 import About from '@/pages/About'
@@ -16,19 +16,19 @@ import BlogLaborCost from '@/pages/BlogLaborCost'
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="services" element={<Services />} />
-        <Route path="pre-opening" element={<PreOpening />} />
-        <Route path="experience" element={<Experience />} />
-        <Route path="philosophy" element={<Philosophy />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="profile" element={<Profile />} />
-        <Route path="blog" element={<BlogIndex />} />
-        <Route path="blog/pre-opening" element={<BlogPreOpening />} />
-        <Route path="blog/fractional-gm" element={<BlogFractionalGM />} />
-        <Route path="blog/labor-cost" element={<BlogLaborCost />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/pre-opening" element={<PreOpening />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/philosophy" element={<Philosophy />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/insights" element={<BlogIndex />} />
+        <Route path="/insights/what-a-fractional-gm-actually-does" element={<BlogFractionalGM />} />
+        <Route path="/insights/miami-pre-opening-playbook" element={<BlogPreOpening />} />
+        <Route path="/insights/labor-cost-control-miami-restaurants" element={<BlogLaborCost />} />
       </Route>
     </Routes>
   )
