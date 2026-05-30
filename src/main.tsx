e8ecb1dbac3fc09d0eaf4e3d6fec0a21bcc7a664
LeeMena <leandermena@me.com>
2026-05-29 20:47:08 -0400

    fix: resolve merge conflict in utils.ts and restore missing closing tags in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-﻿import { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
@@ -9,4 +9,5 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  
+  </StrictMode>
+)
