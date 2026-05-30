a45b550b7cd51d3cf41529a6aba8db55e76591ba
LeeMena <leandermena@me.com>
2026-05-30 05:53:28 -0400

    fix: update main.tsx to use correct app mount id

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,13 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
-export const createApp = ViteSSG(() => (
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
-))
+export const createApp = ViteSSG(
+  () => (
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  ),
+  { rootContainerId: 'app' },
+)
