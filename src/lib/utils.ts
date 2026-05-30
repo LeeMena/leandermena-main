b22fd3e1dde49fe15f3f61e7766e248e2d9e7f91
lee14-coder <leandermena14@gmail.com>
2026-05-29 20:49:33 -0400

    fix: resolve merge conflicts in utils.ts and main.tsx

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import { type ClassValue, clsx } from 'clsx'
-=======
 ﻿import { type ClassValue, clsx } from 'clsx'
->>>>>>> 8c488c2 (fix: add missing src/lib/utils.ts)
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
