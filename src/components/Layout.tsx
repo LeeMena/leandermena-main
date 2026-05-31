5eb65d849e8505b67e57f719e2f27cc187ba039d
LeeMena <leandermena@me.com>
2026-05-31 02:10:01 -0400

    feat: add CommandMenu to Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,12 +2,14 @@ import { Outlet } from 'react-router-dom'
 import Navigation from './Navigation'
 import Footer from './Footer'
 import AnnouncementBar from './AnnouncementBar'
+import CommandMenu from './CommandMenu'
 
 export default function Layout() {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100dvh' }}>
       <AnnouncementBar />
       <Navigation />
+      <CommandMenu />
       <main style={{ flex: 1 }}>
         <Outlet />
       </main>
